a3118c8492e14c35c0cae526f4e7b8bc395a05c2
ad-aasati <atul.asati@aurodata.com>
2022-10-03 13:13:53 +0530

    updated code

--- utils.py
+++ utils.py
@@ -99,7 +99,9 @@ def get_features(driver, thread_arg):
         print("exception in bullet1")
         pass
     try:
-        bullet2 = driver.find_element(By.CLASS_NAME,'_250Jnj').text
+        bullet2 = driver.find_element(By.CLASS_NAME,'_1iWRKW').text
+        if bullet2:
+            bullet2 = bullet2.split("View more")[0]
         print("bullet2:- ", bullet2)
     except:
         print("exception in bullet2")
@@ -120,6 +122,7 @@ def get_features(driver, thread_arg):
 def get_product_details(driver, thread_arg):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     color, in_the_box, model_number, touchscreen, model_name, size, pack_of = [""]*7
+    style_code, closure, fit, fabric, pattern, reversible, collar, fabric_care, suitable_for, hem, other_details, outer_material, ideal_for, occasion, secondary_color, sole_material, season, care_instructions, headphone_type, sales_package, connectivity = [""]*21
            
     # img_lists = soup.find('img',attrs={'class':'_2r_T1I _396QI4'}).get('src')     
     # print("img_list :- ", img_lists)    
@@ -142,7 +145,17 @@ def get_product_details(driver, thread_arg):
                 print("Color:- ", color)
             if not touchscreen and "Touchscreen" == field :
                 touchscreen = value
-                print("Touchscreen:- ", touchscreen)                
+                print("Touchscreen:- ", touchscreen)
+
+            if not headphone_type and "Headphone Type" == field :
+                headphone_type = value
+                print("Headphone Type:- ", headphone_type)
+            if not sales_package and "Sales Package" == field :
+                sales_package = value
+                print("Sales Package:- ", sales_package)
+            if not connectivity and "Connectivity" == field :
+                connectivity = value
+                print("Connectivity:- ", connectivity)                
     except Exception as er:
         print("Exception product table :- ", er)
         pass
@@ -160,7 +173,67 @@ def get_product_details(driver, thread_arg):
                 print("Size:- ", size)
             if not pack_of and "Pack of" in field :
                 pack_of = value.text
-                print("Pack of:- ", pack_of)            
+                print("Pack of:- ", pack_of)
+            if not style_code and "Style Code" in field :
+                style_code = value.text
+                print("Style Code:- ",style_code)
+            if not closure and "Closure" in field :
+                closure = value.text
+                print("Closure:- ",closure)
+            if not fit and "Fit" in field :
+                fit = value.text
+                print("Fit:- ",fit)
+            if not fabric and "Fabric" in field :
+                fabric = value.text
+                print("Fabric:- ",fabric)
+            if not pattern and "Pattern" in field : 
+                pattern = value.text
+                print("Pattern:- ",pattern)
+            if not reversible and "Reversible" in field :
+                reversible = value.text
+                print("Reversible:- ",reversible)
+            if not collar and "Collar" in field :
+                collar = value.text
+                print("Collar:- ",collar)
+            if not color and "Color" in field :
+                color = value.text
+                print("Color:- ",color)
+            if not fabric_care and "Fabric Care" in field :
+                fabric_care = value.text
+                print("Fabric Care:- ",fabric_care)
+            if not suitable_for and "Suitable For" in field :
+                suitable_for = value.text
+                print("Suitable For:- ",suitable_for)
+            if not hem and "Hem" in field :
+                hem = value.text
+                print("Hem:- ",hem)
+            if not other_details and "Other Details" in field :
+                other_details = value.text
+                print("Other Details:- ",other_details)
+            if not outer_material and "Outer material "in field :
+                outer_material = value.text
+                print("Outer material:- ",outer_material)
+            if not model_name and "Model name " in field :
+                model_name = value.text
+                print("Model name:- ",model_name)
+            if not ideal_for and 'Ideal for ' in field :
+                ideal_for = value.text
+                print("Ideal for:- ",ideal_for)
+            if not occasion and "Occasion " in field :
+                occasion = value.text
+                print("Occasion:- ",occasion)
+            if not secondary_color and "Secondary color " in field :
+                secondary_color = value.text
+                print("Secondary color:- ",secondary_color)
+            if not sole_material and "Sole material" in field :
+                sole_material = value.text
+                print("Sole material:- ",sole_material)
+            if not season and "Season" in field:
+                season = value.text
+                print("Season:- ",season)
+            if not care_instructions and "Care instructions " in field :
+                care_instructions = value.text
+                print("Care instructions:-",care_instructions)                    
     except Exception as er:
         print("Exception in product list :- ", er)
         pass    
@@ -171,13 +244,33 @@ def get_product_details(driver, thread_arg):
         "model_name": model_name,
         "size": size,
         "pack_of": pack_of,
-        "touchscreen": touchscreen,       
+        "touchscreen": touchscreen,
+        "style_code" : style_code,
+        "closure" : closure,
+        "fit" : fit,
+        "fabric" : fabric,
+        "pattern" : pattern,
+        "reversible" : reversible,
+        "collar" : collar,
+        "fabric_care" : fabric_care,
+        "suitable_for" : suitable_for,
+        "hem" : hem,
+        "other_details" : other_details,
+        "outer_material" : outer_material,
+        "ideal_for" : ideal_for,
+        "occasion" : occasion,
+        "secondary_color" : secondary_color,
+        "sole_material" : sole_material,
+        "season" : season,
+        "care_instructions" : care_instructions,
+        "headphone_type" : headphone_type,
+        "sales_package" : sales_package,
+        "connectivity" : connectivity,
     }
     return thread_arg
 
-
 def get_date():
-    date = '29/09/2022 18:30:30'
+    date = '03/10/2022 18:30:30'
     date = datetime.datetime.strptime(date, '%d/%m/%Y %H:%M:%S')
-    max_date = date + datetime.timedelta(days=10)
+    max_date = date + datetime.timedelta(days=5)
     return max_date
